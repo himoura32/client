393e32500a2a2c55a1210682c79e1ffc945d68f6
himoura32 <shawn.ahearn@outlook.com>
2022-06-06 20:13:57 -0400

    coded action creators/reducer logic

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import authReducer from './authReducer';
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer,
 });
